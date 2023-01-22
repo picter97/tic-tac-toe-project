571e89824c4e8a755bf9dcf78ac642564367d82b
picter <picter2esolv@gmail.com>
2023-01-22 11:44:33 -0500

    new html and css

---
+++ index.js
@@ -0,0 +1,22 @@
+//create variables
+let text = document.getElementById('text')
+let restartBtn = document.getElementById('restartBtn')
+let inbox = Array.from(document.getElementsByClassName('inbox'))
+ console.log(inbox)
+
+//
+
+
+
+
+//create logic for text inside
+
+
+
+
+
+
+
+//create logic for win / lose 
+
+
