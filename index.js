8d80c26fae38b7e493551700fe4066cccfa0f079
picter <picter2esolv@gmail.com>
2023-01-24 15:26:34 -0500

    start game / inbox clicked function

--- index.js
+++ index.js
@@ -2,8 +2,22 @@
 let text = document.getElementById('text')
 let restartBtn = document.getElementById('restartBtn')
 let inbox = Array.from(document.getElementsByClassName('inbox'))
- console.log(inbox)
 
+const text_o = "O"
+const text_x = "X"
+let currentPlayer = text_x
+let  spaces = Array(9).fill(null)
+console.log(spaces)
+
+const startGame = () => {
+  inbox.forEach(box => box.addEventListener('click',inboxClicked))
+}
+
+function inboxClicked(e){
+  console.log(e.target)
+}
+
+startGame()
 //
 
 
