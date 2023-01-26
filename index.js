9884b9d3322db9d8a1363c2c835c4e04a10aae71
picter <picter2esolv@gmail.com>
2023-01-26 14:13:13 -0500

    games not displaying winner

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ let text = document.getElementById('text')
 let restartBtn = document.getElementById('restartBtn')
 //turns an arraylike and turns it into an array 
 let inbox = Array.from(document.getElementsByClassName('inbox'))
-
+// console.log(inbox)
 const text_o = "O"
 const text_x = "X"
 let currentPlayer = text_x
 //change the array to null= empty spaces
 let  spaces = Array(9).fill(null)
-console.log(spaces)
+//console.log(spaces)
 
 //for eahc square adds an event listener on click 
 const startGame = () => {
@@ -19,17 +19,15 @@ const startGame = () => {
 
 //function when clicked adds X or O
 function inboxClicked(e){
+
   const id = e.target.id
   if(!spaces[id]){
-    spaces[id] = currentPlayer
+    
     e.target.innerText = currentPlayer
 
     currentPlayer = currentPlayer == text_x ? text_o : text_x
   }
 }
-
-
-
 
 //reset button 
 
@@ -43,35 +41,6 @@ function restart() {
   })
   currentPlayer = text_x
 }
-
-
-
-
-//create logic for win / lose 
-
-function playerHasWon() {
-  for (const condition of winningCombos) {
-      let [a, b, c] = condition
-
-      if(spaces[a] && (spaces[a] == spaces[b] && spaces[a] == spaces[c])) {
-          return [a,b,c]
-      }
-  }
-  return false
-}
-
-
-const wincomb = [
-  [0,1,2],
-  [3,4,5],
-  [6,7,8],
-  [0,3,6],
-  [1,4,7],
-  [2,5,8],
-  [0,4,8],
-  [2,4,6]
-
-]
 
 
 startGame()
